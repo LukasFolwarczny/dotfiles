67530f5bef0111766dfbeba3f609967fcf713f6f
Lukáš Folwarczný <lfolwarczny@gmail.com>
2014-09-06 19:17:20 +0200

    setup.sh: root does not have graphical environment

--- setup.sh
+++ setup.sh
@@ -8,21 +8,30 @@ if [ "$1" = "-f" ]; then
 	shift
 fi
 
-TARGET="$1"
+TARGET=${1%/}
 if [ -z $TARGET ]; then
 	echo "Usage: $0 [-f] target_directory"
 	exit 1
 fi
 
-DIRS=(bin .folwar .config .config/luakit .config/awesome .config/awesome/theme .moc .moc/themes)
+DIRS=(bin .folwar .moc .moc/themes)
 
-FILES=(.bash_profile .bashrc .vimrc .xinitrc .Xresources)
+FILES=(.bash_profile .bashrc .vimrc)
+FILES+=(.moc/config .moc/themes/blackwhite)
+
+# Root does not use graphical environment
+if [ "$TARGET" != "/root" ]; then
+
+DIRS+=(.config .config/luakit .config/awesome .config/awesome/theme)
+
+FILES+=(.xinitrc .Xresources)
 FILES+=(bin/awesome-fetch-wallpapers bin/awesome-set-wallpaper bin/trymocp bin/yegor-korzh-downloader)
 AS=.config/awesome
 FILES+=($AS/rc.lua $AS/volume.lua $AS/theme/theme.lua)
 LK=.config/luakit
 FILES+=($LK/rc.lua $LK/adblock_chrome.lua $LK/adblock.lua $LK/binds.lua $LK/globals.lua $LK/modes.lua $LK/theme.lua $LK/webview.lua $LK/window.lua)
-FILES+=(.moc/config .moc/themes/blackwhite)
+
+fi
 
 for di in ${DIRS[@]}; do
 	DI="$TARGET/$di"
